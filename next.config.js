392c3bfeb0fac1b289844e4913bc75a5bc92d0cb
Muhammad Shourov (V4MPIR3) <muhammad.shourov01@gmail.com>
2026-03-22 05:54:55 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/nurapp',
+  assetPrefix: '/nurapp/',
+}
+
+module.exports = nextConfig
